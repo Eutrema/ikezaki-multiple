a4a18421ec8f5bcb8f8b6a2f51463460cef34ea3
Eutrema <eutremasoysauce@gmail.com>
2017-01-27 18:20:07 +0900

    Keijibanクラスの変更

--- keijiban/src/jp/ac/oit/igakilab/dwr/keijiban/Keijiban.java
+++ keijiban/src/jp/ac/oit/igakilab/dwr/keijiban/Keijiban.java
@@ -1,35 +1,32 @@
 package jp.ac.oit.igakilab.dwr.keijiban;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
+
+import org.bson.Document;
 
 public class Keijiban {
 	/**
 	 * データベースに登録されている投稿の一覧を取得します
 	 * @return 投稿の履歴(PostFormの配列)
 	 */
-	public List<PostForm> getMessages(String room){
-		//TODO: 投稿取得メソッドの実装
+    public List<PostForm> getMessages(String room){
+        KeijibanDB db = new KeijibanDB();
 
-		//ダミーデータを作成
-		PostForm p1 = new PostForm();
-		PostForm p2 = new PostForm();
-		Calendar cal = Calendar.getInstance();
-		p1.setName("ryo");
-		p1.setMessage("いまどこー？");
-		p2.setName("kita");
-		p2.setMessage("二度寝した");
+        List<PostForm> messages = new ArrayList<PostForm>();
+        for(Document doc : db.getMessages(room)){
+            PostForm post = new PostForm();
+            post.setName(doc.getString("name"));
+            post.setMessage(doc.getString("message"));
+            post.setTime(doc.getDate("time"));
 
-		p2.setTime(cal.getTime());
-		cal.add(Calendar.MINUTE, -3);
-		p1.setTime(cal.getTime());
+            messages.add(post);
+        }
 
-		List<PostForm> dummy = Arrays.asList(p1, p2);
-
-		//ダミーデータを返却
-		return dummy;
-	}
+        db.closeClient();
+        return messages;
+    }
 
 	/**
 	 * メッセージを新しく投稿します
